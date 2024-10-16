ca12ff57634b11176d8b53fadfc404f0e5d0adbc
ohharper <oharper@uwo.ca>
2024-10-16 14:40:06 -0400

    -m 'add files'

---
+++ Decision Making/While Else.py
@@ -0,0 +1,11 @@
+num=10 
+
+while num > 5: 
+
+    print("Inside the loop, num:", num) 
+
+    num-=1 
+
+else: 
+
+    print("Outside the loop, num:", num)
---
+++ Decision Making/While Loop.py
@@ -0,0 +1,11 @@
+i=2  
+
+while i>0: 
+
+    print("i=",i) 
+
+    i-=1 
+
+  
+
+print("Good bye")
