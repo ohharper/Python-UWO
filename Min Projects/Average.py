bff2a33bf2310e51294eb9a550aef03b32996a57
ohharper <oharper@uwo.ca>
2024-11-02 22:08:15 -0400

    add if

--- Min Projects/Average.py
+++ Min Projects/Average.py
@@ -1,7 +1,9 @@
 def Average(ls):
+    if len(ls) == 0:
+        return "list empty"
     total= sum(ls)
     length= len(ls)
     return total/length
 
-ls =[1,2,3,7]
+ls =[1,4]
 print (Average(ls))
